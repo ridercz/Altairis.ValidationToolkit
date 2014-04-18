d97d985e20b4fd58d220e3706d58250d4a44b2f7
Michal Altair Valášek <michal.valasek@altairis.cz>
2014-04-18 15:54:05 +0200

    Added IcoAttribute and GreaterThanAttribute

---
+++ Altairis.ValidationToolkit/GreaterThanAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Altairis.ValidationToolkit {
+    public class GreaterThanAttribute : ValidationAttribute {
+        public GreaterThanAttribute(string otherPropertyName)
+            : base("{0} must be greater than {1}") {
+            this.OtherPropertyName = otherPropertyName;
+        }
+
+        public string OtherPropertyName { get; private set; }
+
+        public string OtherPropertyDisplayName { get; set; }
+
+        public bool AllowEqual { get; set; }
+
+        public override string FormatErrorMessage(string name) {
+            return string.Format(ErrorMessageString, name, this.OtherPropertyName);
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+            var comparableValue = value as IComparable;
+            var comparableOtherValue = this.GetOtherComparableValue(validationContext);
+
+            // Empty or noncomparable values are valid - let others validate that
+            if (comparableValue == null || comparableOtherValue == null) return ValidationResult.Success;
+
+            var compareResult = comparableValue.CompareTo(comparableOtherValue);
+            if (compareResult == 1 || (this.AllowEqual && compareResult == 0)) {
+                // This property is greater than other property or equal when permitted
+                return ValidationResult.Success;
+            }
+            else {
+                // This property is smaller or equal to the other property
+                if (string.IsNullOrWhiteSpace(this.OtherPropertyDisplayName)) this.OtherPropertyDisplayName = this.OtherPropertyName;
+                return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+            }
+        }
+
+        protected IComparable GetOtherComparableValue(ValidationContext validationContext) {
+            var propertyInfo = validationContext.ObjectType.GetProperty(OtherPropertyName);
+            if (propertyInfo != null) {
+                var otherValue = propertyInfo.GetValue(validationContext.ObjectInstance, null);
+                return otherValue as IComparable;
+            }
+            return null;
+        }
+
+        public override bool RequiresValidationContext {
+            get {
+                return true;
+            }
+        }
+
+    }
+}
---
+++ Altairis.ValidationToolkit/IcoAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Altairis.ValidationToolkit {
+    public class IcoAttribute : ValidationAttribute {
+
+        public IcoAttribute()
+            : base("The field {0} must be valid IČO (identification number of person).") {
+        }
+
+        public override bool IsValid(object value) {
+            // Empty values are valid - use RequiredAttribute instead
+            var s = value as string;
+            if (string.IsNullOrWhiteSpace(s)) return true;
+
+            // IČO is 8 digits, pad with zeroes when not
+            if (!Regex.IsMatch(s, "^[0-9]{1,8}$")) return false;
+            s = s.PadLeft(8, '0');
+
+            // Calculate sum of digits
+            var sum = 0;
+            for (int i = 0; i < 7; i++) {
+                sum += int.Parse(s[i].ToString()) * (8 - i);
+            }
+
+            // Verify checksum number
+            var chs = 11 - (sum % 11);
+            return chs.ToString().EndsWith(s.Substring(7));
+        }
+
+    }
+}
