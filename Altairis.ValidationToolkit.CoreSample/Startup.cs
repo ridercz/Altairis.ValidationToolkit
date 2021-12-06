2d94cf78c50e03165fea5d4fc7d4774287d858d4
Michal Altair Valášek <michal.valasek@altairis.cz>
2021-12-07 00:07:54 +0100

    Organize usings

--- Altairis.ValidationToolkit.CoreSample/Startup.cs
+++ Altairis.ValidationToolkit.CoreSample/Startup.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace Altairis.ValidationToolkit.CoreSample {
     public class Startup {
--- Altairis.ValidationToolkit/EmptyBankCodeValidator.cs
+++ Altairis.ValidationToolkit/EmptyBankCodeValidator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Altairis.ValidationToolkit {
+﻿namespace Altairis.ValidationToolkit {
     public class EmptyBankCodeValidator : IBankCodeValidator {
         public bool Validate(string code) => true;
     }
