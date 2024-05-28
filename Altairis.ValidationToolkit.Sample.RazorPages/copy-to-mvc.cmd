0d73d2d297c038e8d1b6e8b7766cd7bfe9003ed1
Michal Altair Valášek <github@rider.cz>
2024-05-28 08:50:59 +0200

    Added templates for Int128 and UInt128 and made some more semantic.

--- Altairis.ValidationToolkit.Sample.RazorPages/copy-to-mvc.cmd
+++ Altairis.ValidationToolkit.Sample.RazorPages/copy-to-mvc.cmd
@@ -1,2 +1,3 @@
 robocopy /MIR .\wwwroot ..\Altairis.ValidationToolkit.Sample.Mvc\wwwroot
 robocopy /MIR .\Pages\EditorTemplates ..\Altairis.ValidationToolkit.Sample.Mvc\Views\Shared\EditorTemplates
+echo OK
